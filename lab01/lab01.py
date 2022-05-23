4788fc5d121b589806c1da4267750347c2442ebd
mcmunchie <89753143+mcmunchie@users.noreply.github.com>
2022-05-23 11:28:25 -0500

    Added comments

--- lab01/lab01.py
+++ lab01/lab01.py
@@ -1,16 +1,26 @@
 # Code Summary: Lab 1 showing Python environment and basic code file template.
 
 def lab1_averages():
+    '''
+    Accepts an integer and float value. 
+    Calculates and returns the average.
+    '''
     myint = int(input("\n\tPlease enter a whole number: "))
     myfloat = float(input("\tPlease enter a decimal number: "))
     return (myint + myfloat)/2
 
 def lab1_concat():
+    '''
+    Accepts two strings, a first and last name. Concatenates and returns a concatenated string.
+    '''
     first = input("\n\tPlease enter your first name: ").lower()
     last = input("\tPlease enter your last name: ").lower()
     return (last.title() + ", " + first.title())
 
 def main():
+    '''
+    Main driver method that makes function calls to the previous `lab1_concat()` and `lab_1averages()` methods.
+    '''
     print("\nWelcome to the first lab.")
     from_concat = lab1_concat()
     from_average = lab1_averages()
